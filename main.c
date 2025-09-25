63aedba5a38c4271a9bfeaf7f7f7fca231a7031d
seonoleo <mscomy1@gmail.com>
2025-09-25 16:06:40 +0900

    04.5

--- main.c
+++ main.c
@@ -2,13 +2,22 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
-{int  year;
+{   unsigned int x;
+    int  b;
 
-printf("input the year:");
-scanf("%i", &year);
+    printf("input a number:");
+    scanf("%ui", &x);
 
-printf("is the year %i the leap year?: %i\n", year, 
-(year%4==0 && year%100!=0)||(year%400==0));
+    for(b=0; x!=0; x>>=1)
+    {
+             if(x&1)
+             {
+                    b++;
+             }
+    }
+    
+    printf("The result is: %i\n", b);
+
 
 
   system("PAUSE");	
